5480cf09fa703b2b851917d9dd60e85a087ecaee
Erik <105069417+AvalianY@users.noreply.github.com>
2026-05-12 21:08:57 +0200

    Initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import type { Movie } from '../types/movie';
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const token = import.meta.env.VITE_TMDB_TOKEN as string;
+
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+        include_adult: false,
+        language: 'en-US',
+        page: 1,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data.results;
+};
